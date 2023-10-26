e32b121fffa41dfb95500797387d650bca5ea967
Vinodnkr <vinodnowkumar@gamail.com>
2023-10-26 23:33:57 +0530

    Updates

---
+++ precache-manifest.eda212c83aa9ca837cd29b901ae96041.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6698d3e1399cd6d127e26829dabc89",
+    "url": "/interviewloginImage/index.html"
+  },
+  {
+    "revision": "37c83e7622eee60d88f3",
+    "url": "/interviewloginImage/static/css/main.f7feed00.chunk.css"
+  },
+  {
+    "revision": "48bd30ca68f0ada11ead",
+    "url": "/interviewloginImage/static/js/2.138d10ec.chunk.js"
+  },
+  {
+    "revision": "37c83e7622eee60d88f3",
+    "url": "/interviewloginImage/static/js/main.90c9e7fc.chunk.js"
+  },
+  {
+    "revision": "f2b66c73550fefacb428",
+    "url": "/interviewloginImage/static/js/runtime~main.8b627707.js"
+  }
+]);
